55998403fb1e09739110d57e7798d71537061774
KartikKaushik29 <kartik24295@iiitd.ac.in>
2025-02-19 23:22:05 +0530

    Update Assembler.py

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -1,3 +1,50 @@
+import sys
+import re
+
+opcode_mapping = {
+    # R-Type Instructions having formats (opcode, funct3, funct7)
+    "sub":  ("0110011", "000", "0100000"),
+    "add":  ("0110011", "000", "0000000"),
+    "or":   ("0110011", "110", "0000000"),
+    "srl":  ("0110011", "101", "0000000"),
+    "slt":  ("0110011", "010", "0000000"),
+    "xor":  ("0110011", "100", "0000000"),
+
+    # I-Type Instructions having formats (opcode, funct3)
+    "addi": ("0010011", "000"),
+    "lw":   ("0000011", "010"),
+    "jalr": ("1100111", "000"),
+    
+    # S-Type Instructions having formats (opcode, funct3)
+    "sw":   ("0100011", "010"),
+    
+    # B-Type Instructions having formats (opcode, funct3)
+    "beq":  ("1100011", "000"),
+    "bne":  ("1100011", "001"),
+    "blt":  ("1100011", "100"),
+    "bge":  ("1100011", "101"),  
+    "bltu": ("1100011", "110"),
+    
+    # J-Type Instructions having formats (only opcode)
+    "jal":  ("1101111",)
+}
+
+
+register_mapping = {
+    # Special Purpose Registers used for assembler
+    "zero": 0, "ra": 1, "sp": 2, "gp": 3, "tp": 4,
+
+    # Temporary Registers used for assembler
+    "t0": 5, "t1": 6, "t2": 7, "t3": 28, "t4": 29, "t5": 30, "t6": 31,
+
+    # Saved Registers used for assembler
+    "s0": 8, "s1": 9, "s2": 18, "s3": 19, "s4": 20, "s5": 21,
+    "s6": 22, "s7": 23, "s8": 24, "s9": 25, "s10": 26, "s11": 27,
+
+    # Argument Registers used for assembler
+    "a0": 10, "a1": 11, "a2": 12, "a3": 13, "a4": 14,
+    "a5": 15, "a6": 16, "a7": 17
+}
 def assembler_code(output_file,input_file):
     f=open(input_file, 'r')
     lines = f.readlines()
