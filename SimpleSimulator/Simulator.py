5ac84c45875347d1b08122848d85410c7644b8d6
Anilcodee <anil24076@iiitd.ac.in>
2025-04-03 23:47:26 +0530

    Update Simulator.py

--- SimpleSimulator/Simulator.py
+++ SimpleSimulator/Simulator.py
@@ -111,3 +111,79 @@ def simulator(inputf):
                 else:
                     raise Exception(f"unsupported R-type function3={function3}, function7={function7}")
                 win_registers(rgs, rd, res)
+
+            elif op_code == "0010011":  # for I-type instructions (ADDI)
+                imm = s_extender(int(curr_inst[0:12], 2), 12)
+                rs1 = int(curr_inst[12:17], 2)
+                function3 = curr_inst[17:20]
+                rd = int(curr_inst[20:25], 2)
+                if function3 == "000":
+                    res = rgs[rs1] + imm
+                else:
+                    raise Exception(f"unsupported I-type function3: {function3}")
+                win_registers(rgs, rd, res)
+
+            elif op_code == "0000011":  # I-type (LW)
+                imm = s_extender(int(curr_inst[0:12], 2), 12)
+                rs1 = int(curr_inst[12:17], 2)
+                function3 = curr_inst[17:20]
+                rd = int(curr_inst[20:25], 2)
+                if function3 == "010":
+                    addr = rgs[rs1] + imm
+                    value = read_memory(data_mem, stack_mem, addr)
+                    win_registers(rgs, rd, value)
+
+            elif op_code == "0100011":  # for S-type instructions (SW)
+                imm_high = curr_inst[0:7]
+                imm_low = curr_inst[20:25]
+                imm = s_extender(int(imm_high + imm_low, 2), 12)
+                rs1 = int(curr_inst[12:17], 2)
+                rs2 = int(curr_inst[7:12], 2)
+                function3 = curr_inst[17:20]
+                if function3 == "010":
+                    addr = rgs[rs1] + imm
+                    data_mem, stack_mem = write_memory(data_mem, stack_mem, addr, rgs[rs2])
+
+            elif op_code == "1100011":  # for B-type instructions
+                imm = (int(curr_inst[0]) << 12) | (int(curr_inst[24]) << 11) | (int(curr_inst[1:7], 2) << 5) | (int(curr_inst[20:24], 2) << 1)
+                imm = s_extender(imm, 13)
+                rs1 = int(curr_inst[12:17], 2)
+                rs2 = int(curr_inst[7:12], 2)
+                function3 = curr_inst[17:20]
+                branch = False
+                if function3 == "000":
+                    branch = (rgs[rs1] == rgs[rs2])
+                elif function3 == "001":
+                    branch = (rgs[rs1] != rgs[rs2])
+                else:
+                    raise Exception(f"unsupported B-type function3 found: {function3}")
+                new_pc = pc + (imm if branch else 4)
+
+            elif op_code == "1101111":  # JAL
+                imm = (int(curr_inst[0]) << 20) | (int(curr_inst[1:11], 2) << 1) | (int(curr_inst[11]) << 11) | (int(curr_inst[12:20], 2) << 12)
+                imm = s_extender(imm, 21)
+                rd = int(curr_inst[20:25], 2)
+                win_registers(rgs, rd, pc + 4)
+                new_pc = pc + imm
+
+            elif op_code == "1100111":  # JALR
+                imm = s_extender(int(curr_inst[0:12], 2), 12)
+                rs1 = int(curr_inst[12:17], 2)
+                rd = int(curr_inst[20:25], 2)
+                target = (rgs[rs1] + imm) & ~1
+                win_registers(rgs, rd, pc + 4)
+                new_pc = target
+
+            else:
+                raise Exception(f"It is a unknown op_code: {op_code}")
+
+        except Exception as e:
+            print(f"Error present in instruction ln {inst_pos+1} (PC=0x{pc:08X}): {e}")
+            sys.exit(1)
+
+        pc = new_pc
+        b_t.append(ft_binary(pc, rgs))
+        d_t.append(ft_decimal(pc, rgs))
+
+    return (b_t, d_t, data_mem)
+
